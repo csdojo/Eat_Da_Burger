967ba62dee88cece76f44508bd9875b470c09527
csdojo <sherrywanwang@gmail.com>
2018-11-07 15:07:23 -0500

    save

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+
+
+
