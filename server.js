af5562079a8a384ad2f7e51378a16ae4c89c1083
csdojo <sherrywanwang@gmail.com>
2018-11-09 22:54:16 -0500

    dd

--- server.js
+++ server.js
@@ -23,7 +23,4 @@ var routes = require("./controllers/burgersController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(process.env.PORT || 5000)
