77e1fd84c8458be9fa0baa39b2d2bff4eed7b640
csdojo <sherrywanwang@gmail.com>
2018-11-09 21:37:29 -0500

    readme and others

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
-INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
-INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Steak Burger');
+INSERT INTO burgers (name) VALUES ('Patty Melt');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ("McDonald's Big Mac");
 
 
 
